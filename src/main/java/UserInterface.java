87c8d7cfe220c73603944f8de643e75a1f6a6d24
MeH2607 <moha898h@stud.kea.dk>
2022-09-26 13:16:30 +0200

    lavet startProgram og manuel testet brugergrænseflade. Den leder ikke til noget nu

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,2 +1,35 @@
+import java.util.Scanner;
+
 public class UserInterface {
-}
+   private Adventure adventure = new Adventure();
+   private Scanner sc = new Scanner(System.in);
+
+    public void startProgram() {
+        String direction; //TODO lav en readString() til at tage sig af case følsomhed
+
+        System.out.println("Welcome to the game. let's start");
+
+        System.out.println("Choose a direction");
+        direction = sc.nextLine();
+
+        switch (direction) {
+            case "North":
+                System.out.println("Going north");
+                break;
+            case "South":
+                System.out.println("Going south");
+                break;
+            case "East":
+                System.out.println("Going east");
+                break;
+            case "West":
+                System.out.println("going west");
+                break;
+            default:
+                break;
+        }
+        }
+
+    }
+
+
