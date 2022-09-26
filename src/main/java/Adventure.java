a8e8226d97339d2eae4541cb7272c0437d5384ed
paul7299 <paul7299@stud.kea.dk>
2022-09-26 12:39:18 +0200

    3rd commit

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -1,2 +1,14 @@
 public class Adventure {
+
+    Room room1;
+    Room room2;
+
+    public Adventure() {
+        room1 = new Room("Room 1", "The first room.");
+        room2 = new Room("Room 2", "The second room.");
+    }
+
+
+
+
 }
--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,2 +1,22 @@
 public class Room {
+
+    private String roomName;
+    private String roomDescription;
+    private Room roomNorth;
+    private Room roomSouth;
+    private Room roomEast;
+    private Room roomWest;
+
+    public Room(String roomName, String roomDescription) {
+        this.roomName = roomName;
+        this.roomDescription = roomDescription;
+    }
+
+    public void setRooms(Room roomNorth, Room roomSouth, Room roomEast, Room roomWest) {
+        roomNorth = roomNorth;
+        roomSouth = roomSouth;
+        roomEast = roomEast;
+        roomWest = roomWest;
+    }
+
 }
