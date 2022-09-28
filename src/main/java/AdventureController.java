1e00a03c6b24041ab97242aaf36414146abe155f
Asgertor <asge1114@stud.kea.dk>
2022-09-28 21:14:26 +0200

    Moved room creation to Adventure_creator

--- src/main/java/AdventureController.java
+++ src/main/java/AdventureController.java
@@ -4,16 +4,6 @@ public class AdventureController extends Player {
     //TODO Mohamed currentRoom flyttes til player. Alle steder man kalder currentroom, skal man i stedet kalde player.getCurrentRoom.
 
     //TODO Asger de 9 rum til adventure creator
-    Room room1;
-    Room room2;
-    Room room3;
-    Room room4;
-    Room room5;
-    Room room6;
-    Room room7;
-    Room room8;
-    Room room9;
-
     Player player;
 
     public AdventureController() {
@@ -32,29 +22,6 @@ public class AdventureController extends Player {
     //i metoden refererer vi til room1 med this
 
     //TODO Asger flyt createMap() til adventureCreator
-    public void createMap() {
-        room1 = new Room("Room 1", "The first room.");
-        room2 = new Room("Room 2", "The second room.");
-        room3 = new Room("Room 3", "The third room.");
-        room4 = new Room("Room 4", "The fourth room.");
-        room5 = new Room("Room 5", "The fifth room.");
-        room6 = new Room("Room 6", "The sixth room.");
-        room7 = new Room("Room 7", "The seventh room.");
-        room8 = new Room("Room 8", "The eight room.");
-        room9 = new Room("Room 9", "The ninth room.");
-
-        room1.setRooms(null, room4, room2, null);
-        room2.setRooms(null, null, room3, room1);
-        room3.setRooms(null, room6, null, room2);
-        room4.setRooms(room1, room7, null, null);
-        room5.setRooms(null, room8, null, null);
-        room6.setRooms(room3, room9, null, null);
-        room7.setRooms(room4, null, room8, null);
-        room8.setRooms(room5, null, room9, room7);
-        room9.setRooms(room6, null, null, room8);
-
-        this.player.currentRoom = room1;
-    }
 
     //Opdaterer status for om man har besøgt et rum. True = Har besøgt et rum
 
--- src/main/java/Adventure_creator.java
+++ src/main/java/Adventure_creator.java
@@ -2,5 +2,40 @@ public class Adventure_creator {
 
     //TODO Asger indsæt rum 1-9
     //TODO Asger indsæt createMap()
+    Room room1;
+    Room room2;
+    Room room3;
+    Room room4;
+    Room room5;
+    Room room6;
+    Room room7;
+    Room room8;
+    Room room9;
+    Player player;
+
+    public void createMap() {
+        room1 = new Room("Room 1", "The first room.");
+        room2 = new Room("Room 2", "The second room.");
+        room3 = new Room("Room 3", "The third room.");
+        room4 = new Room("Room 4", "The fourth room.");
+        room5 = new Room("Room 5", "The fifth room.");
+        room6 = new Room("Room 6", "The sixth room.");
+        room7 = new Room("Room 7", "The seventh room.");
+        room8 = new Room("Room 8", "The eight room.");
+        room9 = new Room("Room 9", "The ninth room.");
+
+        room1.setRooms(null, room4, room2, null);
+        room2.setRooms(null, null, room3, room1);
+        room3.setRooms(null, room6, null, room2);
+        room4.setRooms(room1, room7, null, null);
+        room5.setRooms(null, room8, null, null);
+        room6.setRooms(room3, room9, null, null);
+        room7.setRooms(room4, null, room8, null);
+        room8.setRooms(room5, null, room9, room7);
+        room9.setRooms(room6, null, null, room8);
+
+        this.player.currentRoom = room1;
+
+    }
 
 }
