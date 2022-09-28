107d8cefe641e73a7457ef9a1ba05a3b7bbee1db
MeH2607 <moha898h@stud.kea.dk>
2022-09-28 13:29:10 +0200

    SMå rettelser i adventureController

--- src/main/java/AdventureController.java
+++ src/main/java/AdventureController.java
@@ -60,27 +60,27 @@ public class AdventureController extends Player {
 
     //Skal bruges senere til døre
     public void getCurrentRoomDoors() {
-        if (player.getCurrentRoom.getRoomNorth() != null) {
+        if (player.getCurrentRoom().getRoomNorth() != null) {
             System.out.println("There is a door north");
         }
-        if (player.getCurrentRoom.getRoomSouth() != null) {
+        if (player.getCurrentRoom().getRoomSouth() != null) {
             System.out.println("There is a door south");
         }
-        if (player.getCurrentRoom.getRoomEast() != null) {
+        if (player.getCurrentRoom().getRoomEast() != null) {
             System.out.println("There is a door east");
         }
-        if (player.getCurrentRoom.getRoomWest() != null) {
+        if (player.getCurrentRoom().getRoomWest() != null) {
             System.out.println("There is a door west");
         }
     }
 
     public void setHasVisitedStatusToTrue() {
-        player.getCurrentRoom.setHasVisitedToTrue();
+        player.getCurrentRoom().setHasVisitedToTrue();
     }
 
     //bruges til at tjekke om man har besøgt et rum før
     public Boolean hasVisitedStatus() {
-        return player.getCurrentRoom.getHasVisited();
+        return player.getCurrentRoom().getHasVisited();
     }
 
 }
